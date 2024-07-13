2464db2e46ef12b78113593767688d9bfbb80334
wen zhang <iwenyou@gmail.com>
2024-07-12 20:31:31 -0700

    added more default value

--- src/components/DefaultValues.vue
+++ src/components/DefaultValues.vue
@@ -4,23 +4,39 @@
     <form @submit.prevent="saveDefaults">
       <div>
         <label for="height">Height:</label>
-        <input type="number" v-model.number="defaults.height" required />
+        <input type="number" v-model.number="defaults.height" step="0.01" required />
       </div>
       <div>
         <label for="width">Width:</label>
-        <input type="number" v-model.number="defaults.width" required />
+        <input type="number" v-model.number="defaults.width" step="0.01" required />
       </div>
       <div>
         <label for="thickness">Thickness:</label>
-        <input type="number" v-model.number="defaults.thickness" required />
+        <input type="number" v-model.number="defaults.thickness" step="0.01" required />
       </div>
       <div>
         <label for="number">Number:</label>
-        <input type="number" v-model.number="defaults.number" required />
+        <input type="number" v-model.number="defaults.number" step="0.01" required />
       </div>
       <div>
         <label for="coefficient">Coefficient:</label>
-        <input type="number" v-model.number="defaults.coefficient" required />
+        <input type="number" v-model.number="defaults.coefficient" step="0.01" required />
+      </div>
+      <div>
+        <label for="shippingRate">Shipping Rate:</label>
+        <input type="number" v-model.number="defaults.shippingRate" step="0.01" required />
+      </div>
+      <div>
+        <label for="importTaxRate">Import Tax Rate:</label>
+        <input type="number" v-model.number="defaults.importTaxRate" step="0.01" required />
+      </div>
+      <div>
+        <label for="storageFee">Storage Fee:</label>
+        <input type="number" v-model.number="defaults.storageFee" step="0.01" required />
+      </div>
+      <div>
+        <label for="exchangeRate">Exchange Rate:</label>
+        <input type="number" v-model.number="defaults.exchangeRate" step="0.01" required />
       </div>
       <button type="submit">Save Defaults</button>
     </form>
@@ -39,7 +55,11 @@ export default {
         width: 0,
         thickness: 0,
         number: 0,
-        coefficient: 0
+        coefficient: 0,
+        shippingRate: 0,
+        importTaxRate: 0,
+        storageFee: 0,
+        exchangeRate: 0
       },
       message: ''
     };
@@ -65,7 +85,13 @@ export default {
             width: 0,
             thickness: 0,
             number: 0,
-            coefficient: 0
+            coefficient: 0,
+            shippingRate: 0,
+            importTaxRate: 0,
+            storageFee: 0,
+            exchangeRate: 0,
+            profitMargin: 0,
+            discountRate: 0
           };
         }
       } catch (err) {
